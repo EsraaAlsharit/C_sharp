ee04613810c04b654cfa15019a971e79b8d2c0be
esraa alsharit <esraa.alsharit@gmail.com>
2023-12-01 05:08:50 +0300

    ridemader

---
+++ Language Fundamentals/OOP With C#/Ride Mader/RideMader/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+
+namespace RideMader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vehicle
+            //how to link files
+            // Vehicle myObj = new Vehicle("Honda", "Gray");
+            // Vehicle myObj2 = new Vehicle("Honda", "Red", 2, true);
+            // myObj.ShowInfo();
+
+            // myObj2.ShowInfo();
+            // myObj2.Travel(100);
+            // myObj2.ShowInfo();
+
+            List<Vehicle> Cars = new List<Vehicle>();
+            Cars.Add(new Vehicle("Honda", "Gray"));
+            Cars.Add(new Vehicle("Honda", "Red", 2, true));
+            Cars.Add(new Vehicle("Rollerblades", "White", 8, true));
+            Cars.Add(new Vehicle("Mountain Bike", "White", 5, true));
+
+            foreach (Vehicle Car in Cars)
+            {
+                Car.ShowInfo();
+            }
+            Console.WriteLine();
+
+            Cars[3].Travel(100);
+            Cars[3].ShowInfo();
+
+            //is inaccessible due to its protection level
+            // Cars[1].Miles= 350;
+            // Cars[1].ShowInfo();
+
+        }
+    }
+}
