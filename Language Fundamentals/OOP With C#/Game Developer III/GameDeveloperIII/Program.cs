b9f92f6c019fbb4a4df9bf664ce259848a0f5931
esraa alsharit <esraa.alsharit@gmail.com>
2023-12-23 19:23:00 +0300

    oop with c#

---
+++ Language Fundamentals/OOP With C#/Game Developer III/GameDeveloperIII/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace GameDeveloperIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you name: ");
+
+            string Name = Console.ReadLine();
+            // Convert.ToInt32(NumberInput);
+
+            Console.WriteLine($"Welcome {Name}");
+
+
+
+        }
+    }
+
+}
---
+++ Language Fundamentals/OOP With C#/Readme.md
@@ -0,0 +1,8 @@
+# OOP With C#
+
+- [x] 📝Ride Maker
+- [x] 📝Game Developer I
+- [x] 📝Drink Maker
+- [x] 📝Game Developer II
+- [ ] 📝Fuel Up!
+- [ ] 📝Game Developer III
