b06e1da9e9a9f59f0a75663cc569308f2fba6d05
Esraa Alsharit <47732120+EsraaAlsharit@users.noreply.github.com>
2024-05-14 22:52:00 +0300

    wedding planner

--- ORMs/Belt Prep/Wedding Planner/WeddingPlanner/obj/Debug/net8.0/WeddingPlanner.AssemblyInfo.cs
+++ ORMs/Belt Prep/Wedding Planner/WeddingPlanner/obj/Debug/net8.0/WeddingPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeddingPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d5b69128e158a526a7a3ea1dfb1693cf3c3bb3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d279244a5d4e2dba460a04f940186d071f6dceaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeddingPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeddingPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
